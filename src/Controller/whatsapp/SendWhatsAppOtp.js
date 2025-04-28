3ecebb8405a45dcd9b3c7aa71b163120cf887ae7
rajanalibabaa <rajanalibabaa@gmail.com>
2025-04-28 15:35:42 +0530

    full check file

--- src/Controller/whatsapp/SendWhatsAppOtp.js
+++ src/Controller/whatsapp/SendWhatsAppOtp.js
@@ -1,3 +1,4 @@
+import { generateOTP } from "../../Utils/generateOTP.js";
 import sendWhatsappTwiloOtp from "../../Utils/sendWhatsappTwiloOtp.js";
 
 const sendOTPController = async (req, res) => {
@@ -6,10 +7,10 @@ const sendOTPController = async (req, res) => {
   if (!phoneNumber) {
     return res.status(400).json({ success: false, message: "Phone number is required" });
   }
-
+const otp =generateOTP();
   try {
-    const response = await sendWhatsappTwiloOtp(phoneNumber);
-    res.status(200).json({ success: true, message: "OTP sent via WhatsApp", data: response });
+    const response = await sendWhatsappTwiloOtp(phoneNumber,otp);
+    res.status(200).json({ success: true, message: "OTP sent via WhatsApp", data: {sid:response.sid,otp} });
   } catch (error) {
     res.status(500).json({ success: false, message: "Failed to send OTP", error: error.message });
   }
--- src/Utils/sendWhatsappTwiloOtp.js
+++ src/Utils/sendWhatsappTwiloOtp.js
@@ -5,20 +5,45 @@ dotenv.config();
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN,);
 const senderWhatsAppNumber=process.env.TWILIO_PHONE_NUMBER
 // Send OTP via WhatsApp
-const sendWhatsAppOTP = async (phoneNumber) => {
+const sendWhatsAppOTP = async (phoneNumber,otp) => {
   console.log("Sending OTP via WhatsApp to:", phoneNumber)
-  
-  return client.verify.v2.services(process.env.TWILIO_SERVICE_SID)
-
-    .verifications
-    .create({
-      from: `whatsapp:${senderWhatsAppNumber}`,
+  try {
+    const message = await client.messages.create({
+      from: 'whatsapp:' + senderWhatsAppNumber,
       to: `whatsapp:${phoneNumber}`,
-      channel: 'whatsapp'
-    })
-    .then(message => console.log(message.sid))
-    .catch(error => console.error(error));
+      contentSid: process.env.TWILIO_TEMPLATE_SID, // your approved template SID
+      channel: 'whatsapp',
+      contentVariables: JSON.stringify({
+        // Your template variables if required
+        "1": otp     
+       })
+    });
+
+    console.log('Message SID:', message.sid);
+    console.log(otp);
+    
+    return message;
+  } catch (error) {
+    console.error('Error sending WhatsApp template:', error);
+    throw error;
+  }
 };
+
+  
+//   return client.verify.v2.services(process.env.TWILIO_SERVICE_SID)
+
+//     .verifications
+//     .create({
+//       from: `whatsapp:${senderWhatsAppNumber}`,
+//       to: `whatsapp:${phoneNumber}`,
+//       body:"",
+//       contentSid:'HXdc76d37b0ecd6e9370a26dfc25864e71',
+//       // contentVariables: JSON.stringify(templateParams),
+//       channel: 'whatsapp'
+//     })
+//     .then(message => console.log(message.sid))
+//     .catch(error => console.error(error));
+// };
 
 
 
